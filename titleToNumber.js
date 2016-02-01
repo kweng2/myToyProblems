9eb8ed2dae368a0dda88e8ed32b63102c259fc2d
kweng2 <kweng2@gmail.com>
2016-02-01 10:37:08 -0800

    titleToNumber complete

---
+++ titleToNumber.js
@@ -0,0 +1,32 @@
+// Related to question Excel Sheet Column Title
+
+// Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+// For example:
+
+//     A -> 1
+//     B -> 2
+//     C -> 3
+//     ...
+//     Z -> 26
+//     AA -> 27
+//     AB -> 28 
+// charcode of A is 65
+
+/**
+ * @param {string} s
+ * @return {number}
+*/
+
+var titleToNumber = function(s) {
+  var output = 0;
+  counter = 0;
+  for ( var i = s.length-1; i >= 0; i-- ) {
+    // console.log(s[i], s.charCodeAt(i), s.charCodeAt(i)-64+(i*26));
+    // output += (s.charCodeAt(i) - 64) + (i*25)*(s.charCodeAt(i) - 64);
+    output += Math.pow(26, counter)*(s.charCodeAt(i) - 64);
+    counter++;
+  }
+  return output;
+};
+
